b67ecbc97b99752d1e7c22e5f595548c23617cc0
Pjotr Prins <pjotr.public01@thebird.nl>
2010-01-03 11:10:21 +0100

    Find Bioruby test data dir

---
+++ lib/bio/test/biotestfile.rb
@@ -0,0 +1,15 @@
+# 
+# biotestfile.rb
+#
+# Copyright (C) 2010 Pjotr Prins <pjotr.prins@thebird.nl> 
+#
+
+module BioTestFile
+
+  TEST_DATA = "#{ENV['BIORUBY_HOME']}/test/data"
+
+  # Read +fn+ from the test data directory
+  def read fn
+    File.read(TEST_DATA+'/'+fn)
+  end
+end
